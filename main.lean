c5bd006d883b9b896ee4e752023fe9e67acb3316
kwurbis <simon.l.strzoda@gmail.com>
2023-04-08 23:17:40 +0200

    started working on the less_than relation

--- main.lean
+++ main.lean
@@ -40,11 +40,11 @@ rw a_ih,
 rw add_succ}
 end
 
-#reduce my_mul zero.succ.succ zero.succ.succ.succi
-
 def my_mul : mynat -> mynat -> mynat
 | zero b := zero
 | (succ a) b := my_add b (my_mul a b)
+
+#reduce my_mul zero.succ.succ zero.succ.succ.succ
 
 lemma mul_zero (a : mynat) : my_mul a zero = zero :=
 begin
@@ -69,8 +69,29 @@ end
 def less_eq_than: mynat → mynat → Prop
 | zero a := true
 | (succ a) zero := false
-| (succ a) (succ b) := less_eq_than a b   
+| (succ a) (succ b) := less_eq_than a b
+ 
+-- proofing less_eq_than is decidable
+instance less_eq_than_decidable (a b : mynat) : decidable (less_eq_than a b):=
+begin
+induction a,
+{rw less_eq_than, apply_instance, --warum löst apply_instance das Problem???
+-- was macht diese Taktik?
+},
+{
+ sorry -- der Induktionschritt kann nicht bewiesen werden da er falsch ist --> anderen Weg suchen
+}
+end
 
  #reduce less_eq_than (succ (succ zero)) (succ zero)
+
+-- define division with rest (dividen -> mod -> rest)
+def div_rest (x m : mynat) : mynat := 
+if (less_eq_than x m) then
+  x
+else
+  x
+
+#eval div_rest zero.succ zero.succ.succ
 
 end mynat
